7919056884d85f352abba8eaddc373b93c21645d
Danhle <lexuandanh1001@gmail.com>
2023-02-27 16:58:30 +0700

    add file gitignore

---
+++ src/NorthwindApp.Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Northwind.Data.Entities;
+
+namespace Northwind.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        public DbSet<EmployeeEntity> Employees { get; set; }
+    }
+}
---
+++ src/NorthwindApp.Data/Entities/BaseEntity.cs
@@ -0,0 +1,7 @@
+﻿namespace Northwind.Data.Entities
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
